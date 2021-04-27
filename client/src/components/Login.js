fd80a5b921be6a227de11091dfe1816ca7ab1c1f
patilharss <patilharss@gmail.com>
2021-04-27 13:33:18 +0530

    added a simple login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,54 @@
 import React from 'react'
+import alumni from "../images/alumni1.jpg"
+import { NavLink, Route } from 'react-router-dom'
+
 const Login =()=>{
     return(
-    <div>
-        
-        <p>WELCOME</p>
-        <h1>login</h1>
-    </div>
+   <>
+   <section className="sign-in">
+            <div className="container mt-5">
+                <div className="signin-content">
+                <div className="signin-img">
+                            <figure>
+                                <img src={alumni} alt="register-pic"/>
+                            </figure>
+                            <NavLink to="/signup" className="signin-image-link">Create an account</NavLink>
+
+                        </div>
+                        
+                    <div className="signin-form">
+                        <h2 className="form-title">Sign up</h2>
+                        <form className="register-form" id="register-form">
+
+                        
+                            <div className="form-group>">
+                                <label htmlFor="email">
+                                <i class="zmdi zmdi-email" style={{marginRight:1+ 'em'}}></i>
+                                </label>
+                                <input type="text" name="email" id="email" autoComplete="off" placeholder="Email"/>
+                            </div>
+                            <div className="form-group>">
+                                <label htmlFor="password">
+                                <i class="zmdi zmdi-lock" style={{marginRight:1+ 'em'}}></i>
+                                </label>
+                                <input type="text" name="password" id="password" autoComplete="off" placeholder="Password"/>
+                            </div>
+                            <div className="form-group form-button">
+                                <input type="submit" name="signin" id="signin" className="form-submit" value="Sign in"/>
+
+
+                            </div>
+                            
+                        </form>
+                        </div>
+                </div>
+            </div> 
+        </section>
+
+    
+
+
+   </>
 
     )
 }
