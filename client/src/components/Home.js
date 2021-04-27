55b2944b556a5f366fa85d03d57310bc66d5e5ae
patilharss <patilharss@gmail.com>
2021-04-28 01:40:17 +0530

    Added dynamic home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 const Home =()=>{
+
+    //check if user is login
+    const [userName,setUserName] = useState('');//getting name in userName beacuse we need only that
+
+    const [show,setShow]=useState(false);
+    
+
+    const userContact = async ()=>{
+        try{
+            const res= await fetch('/getdata',{
+                mathod:'GET',
+                headers:{
+                    "Content-Type":"application/json"
+                },
+            });
+            const data=await res.json();
+            console.log(data)
+            setUserName(data.firstname,data.lastname);
+            setShow(true);
+           
+
+            
+
+            if (!res.status===200){
+                const error=new Error(res.error);
+                throw error;
+            }
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+    useEffect(()=>{
+            userContact();
+    },[]);
     return(
     <>
         <div className="home-page">
             <div className="home-div">
-                <p className="pt-5">Welcome</p>
+                <p className="pt-5"></p>
+                <h1>{userName}</h1>
+                <h2>{show ? ' Welcome Back' :'Please Sign in to see your home page' }</h2>
                 <h1>Sanjay Ghodawat University Alumni</h1>
             </div>
 
