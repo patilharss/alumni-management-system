a24119c100e8d15a06a0033ecaba698745847004
patilharss <patilharss@gmail.com>
2021-04-27 13:43:52 +0530

    implemented login system, cookies created and stored

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React,{useState} from 'react'
 import alumni from "../images/alumni1.jpg"
-import { NavLink, Route } from 'react-router-dom'
+import { NavLink, useHistory } from 'react-router-dom'
 
 const Login =()=>{
+    const history=useHistory();
+    const[email,setEmail]=useState('');
+    const[password,setPassword]=useState('');
+
+
+    const loginUser=async (e)=>{
+        e.preventDefault();
+        const res=await fetch('/signin',{
+            method:'POST',
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                email,
+                password
+
+            })
+        });
+
+        const data=res.json();
+
+        if(res.status===400||!data){
+            window.alert("Invalid credential")
+        }else{
+            window.alert("Login sucessful")
+            history.push('/')
+
+        }
+    }
+
+
     return(
    <>
    <section className="sign-in">
@@ -18,24 +49,25 @@ const Login =()=>{
                         
                     <div className="signin-form">
                         <h2 className="form-title">Sign up</h2>
-                        <form className="register-form" id="register-form">
+                        <form method='POST' className="register-form" id="register-form">
 
                         
                             <div className="form-group>">
                                 <label htmlFor="email">
                                 <i class="zmdi zmdi-email" style={{marginRight:1+ 'em'}}></i>
                                 </label>
-                                <input type="text" name="email" id="email" autoComplete="off" placeholder="Email"/>
+                                <input type="text" name="email" id="email" autoComplete="off" value={email} onChange={(e)=> setEmail(e.target.value)} placeholder="Email"/>
                             </div>
                             <div className="form-group>">
                                 <label htmlFor="password">
                                 <i class="zmdi zmdi-lock" style={{marginRight:1+ 'em'}}></i>
                                 </label>
-                                <input type="text" name="password" id="password" autoComplete="off" placeholder="Password"/>
+                                <input type="password" name="password" id="password" autoComplete="off" value={password} onChange={(e)=> setPassword(e.target.value)} placeholder="Password"/>
                             </div>
-                            <div className="form-group form-button">
-                                <input type="submit" name="signin" id="signin" className="form-submit" value="Sign in"/>
 
+                            <div className="form-group form-button">
+                                <input type="submit" name="signin" id="signin" className="form-submit" value="Sign in" onClick={loginUser}/>
+                                
 
                             </div>
                             
